b0fb90b7725c238a6c630cced460123acd73c197
Ryan Grosso <ryan.grosso@btinternet.com>
2020-11-05 20:38:27 +0000

    base assets added and about initial layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,25 @@
 <template>
   <div>
-    <Nuxt />
+    <div class="content">
+      <AppMenu/>
+      <Nuxt />
+    </div>
   </div>
 </template>
+
+<script>
+import AppMenu from '@/components/AppMenu.vue';
+export default {
+  components: {
+    AppMenu
+  }
+}
+</script>
 
 <style>
 html {
   font-family:
-    'Source Sans Pro',
+    'Mulish',
     -apple-system,
     BlinkMacSystemFont,
     'Segoe UI',
@@ -15,6 +27,7 @@ html {
     'Helvetica Neue',
     Arial,
     sans-serif;
+  font-weight:300;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -31,32 +44,15 @@ html {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+h1, h2, h3,h4 {
+  font-family: "Poppins", -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  font-weight:600;
 }
 </style>
