3576a532d1904c3070d9b92a7d117eac984bc296
Ryan Grosso <ryan.grosso@btinternet.com>
2020-11-12 11:58:09 +0000

    filter added

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,27 @@
 <template>
   <div>
     <main class="container">
-      <AppHeader/>
-      <AppRestaurantInfo />
+      <AppHeader />
+      <AppRestaurantInfo :datasource="fooddata" />
     </main>
   </div>
 </template>
 
 <script>
-import AppHeader from '@/components/AppHeader.vue';
-import AppRestaurantInfo from '@/components/AppRestaurantInfo.vue';
+import AppHeader from "@/components/AppHeader.vue";
+import AppRestaurantInfo from "@/components/AppRestaurantInfo.vue";
+import { mapState } from "vuex";
 
 export default {
   components: {
     AppHeader,
     AppRestaurantInfo,
   },
+  computed: {
+    ...mapState(["fooddata"]),
+  },
 };
 </script>
 
 <style>
-
 </style>
--- pages/restaurants.vue
+++ pages/restaurants.vue
@@ -1,23 +1,46 @@
 <template>
-    <main class="container restaurant">
-        <h1 class="header">Restaurants</h1>
-        <AppRestaurantInfo />
-    </main>
+  <main class="container restaurant">
+    <div class="restaurantheading">
+      <h1 class="header">Restaurants</h1>
+      <AppSelect @change="selectedRestaurant = $event" />
+    </div>
+    <AppRestaurantInfo :datasource="filteredRestaurants" />
+  </main>
 </template>
 
 <script>
-   import AppRestaurantInfo from '@/components/AppRestaurantInfo.vue';
-   
-   export default {
-       components: {
-           AppRestaurantInfo
-       },
-   };
+import AppRestaurantInfo from "@/components/AppRestaurantInfo.vue";
+import AppSelect from "@/components/AppSelect.vue";
+import { mapState } from "vuex";
+
+export default {
+  components: {
+    AppRestaurantInfo,
+    AppSelect,
+  },
+  data() {
+    return {
+      selectedRestaurant: "",
+    };
+  },
+  computed: {
+    ...mapState(["fooddata"]),
+    filteredRestaurants() {
+      if (this.selectedRestaurant) {
+        return this.fooddata.filter((el) => {
+          let name = el.name.toLowerCase();
+          return name.includes(this.selectedRestaurant);
+        });
+        return this.fooddata;
+      }
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .header {
-    text-align: center;
-    padding: 20px 0;
+  text-align: center;
+  padding: 20px 0;
 }
 </style>
